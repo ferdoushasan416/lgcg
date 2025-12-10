50d2a222772d974f2137848cbc60268e15fa28c0
faisal <faisaldevbhaluka@gmail.com>
2025-12-11 00:51:32 +0600

    now create a website for html css and flex box property design

---
+++ assets/js/scripts.js
@@ -0,0 +1,6 @@
+   $(document).ready(function() {
+        // Your jQuery code goes here
+        $('.menu-trigger').on(click,function() {
+            $('.hamburger-menu').addClass('isOpen');
+        });
+    });
